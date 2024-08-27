2195383957b7536325c3694f7f459bf9741eb26f
David Kagan <david.kagan07@gmail.com>
2024-08-27 12:24:28 -0400

    Implemented the count_chars function

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     word_count = get_word_count(text)
     print(word_count)
 
+    book_letter_count = count_chars(text)
+    print(book_letter_count)
+
 
 def get_book_text(path):
     """get_book_text returns the text of a book"""
@@ -19,6 +22,22 @@ def get_word_count(s):
     """get_word_count gets the word count for a string"""
     words = s.split()
     return len(words)
+
+
+def count_chars(s):
+    """count_chars counts how many characters there are in a string
+    of note, this does not distinguish between upper case and lower case
+    letters"""
+
+    chars_dict = {}
+
+    for letter in s:
+        letter = letter.lower()
+        if letter not in chars_dict:
+            chars_dict[letter] = 0
+        chars_dict[letter] += 1
+
+    return chars_dict
 
 
 main()
